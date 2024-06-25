95ffd6f4dae97e824b362826d29f6c289b37baae
Dev_Resty <38154413+tyser1995@users.noreply.github.com>
2024-06-25 08:32:26 +0800

    Install astro DB

---
+++ db/config.ts
@@ -0,0 +1,27 @@
+import { defineDb, defineTable, column } from 'astro:db';
+
+const Author = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text(),
+  }
+});
+
+const Comment = defineTable({
+  columns: {
+    authorId: column.number({ references: () => Author.columns.id }),
+    body: column.text(),
+    // A whole integer value.
+    likes: column.number(),
+    // A true or false value.
+    flagged: column.boolean(),
+    // Date/time values queried as JavaScript Date objects.
+    published: column.date(),
+    // An untyped JSON object.
+    metadata: column.json(),
+  }
+});
+
+export default defineDb({
+  tables: { Comment },
+})
---
+++ src/lib/database.ts
@@ -0,0 +1,10 @@
+// import { createConnection } from 'astro-db';
+
+// const connection = createConnection({
+//   host: 'localhost',
+//   user: 'your-username',
+//   password: 'your-password',
+//   database: 'your-database-name',
+// });
+
+// export default connection;
