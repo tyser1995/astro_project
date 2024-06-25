c57df46afaef8b37619cd5478c59879ffbb80646
Dev_Resty <38154413+tyser1995@users.noreply.github.com>
2024-06-25 20:30:07 +0800

    Remove temporarily

--- src/pages/api/comments/[id].ts
+++ src/pages/api/comments/[id].ts
@@ -1,18 +1,2 @@
 import { db, Comment, isDbError } from 'astro:db';
 import type { APIRoute } from 'astro';
-
-export const POST: APIRoute = (ctx) => {
-  try {
-    await db.insert(Comment).values({
-      id: ctx.params.id,
-      content: 'Hello, world!'
-    });
-  } catch (e) {
-    if (isDbError(e)) {
-      return new Response(`Cannot insert comment with id ${id}\n\n${e.message}`, { status: 400 });
-    }
-    return new Response('An unexpected error occurred', { status: 500 });
-  }
-
-  return new Response(null, { status: 201 });
-};
