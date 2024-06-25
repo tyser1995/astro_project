1e89666f9a94a5e6a716d582adda3e9cc4058146
Dev_Resty <38154413+tyser1995@users.noreply.github.com>
2024-06-25 20:24:54 +0800

    Initialize astro db

--- db/config.ts
+++ db/config.ts
@@ -3,25 +3,23 @@ import { defineDb, defineTable, column } from 'astro:db';
 const Author = defineTable({
   columns: {
     id: column.number({ primaryKey: true }),
-    name: column.text(),
+    name: column.text({
+      optional:true
+    }),
   }
 });
 
 const Comment = defineTable({
   columns: {
-    authorId: column.number({ references: () => Author.columns.id }),
-    body: column.text(),
-    // A whole integer value.
-    likes: column.number(),
-    // A true or false value.
-    flagged: column.boolean(),
-    // Date/time values queried as JavaScript Date objects.
-    published: column.date(),
-    // An untyped JSON object.
-    metadata: column.json(),
+    authorId: column.number({
+      optional:true
+    }),
+    body: column.text({
+      optional:true
+    })
   }
 });
 
 export default defineDb({
-  tables: { Comment },
+  tables: { Author, Comment },
 })
